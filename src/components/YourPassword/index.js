094ae875dd34e2e7878e666c683f09483565048a
S. Mohamed Adnan <62370865+adnansumi98@users.noreply.github.com>
2024-05-01 21:04:14 +0530

    cleaned up code and changed to react best practices

--- src/components/YourPassword/index.js
+++ src/components/YourPassword/index.js
@@ -33,18 +33,19 @@ const PasswordItem = (props) => {
         ) : (
           <img
             src="https://assets.ccbp.in/frontend/react-js/password-manager-stars-img.png"
-            alt="password"
+            alt="stars"
             className="password-display"
           />
         )}
       </div>
       <div className="delete-container">
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/password-manager-delete-img.png"
-          alt="delete"
-          className="delete-button"
-          onClick={onDelete}
-        />
+        <button data-testid="delete" onClick={onDelete}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/password-manager-delete-img.png"
+            alt="delete"
+            className="delete-button"
+          />
+        </button>
       </div>
     </li>
   )
